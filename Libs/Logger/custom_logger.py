c40e90c236ce540814129401dfb237cc37c2ae6e
CausalFlops <88380337+Adarsh-Menon@users.noreply.github.com>
2026-01-19 14:39:18 +0000

    feat: enhance logging capabilities and add configuration management

--- Libs/Logger/custom_logger.py
+++ Libs/Logger/custom_logger.py
@@ -6,14 +6,18 @@ import structlog
 
 class CustomLogger:
     def __init__(self, log_dir="logs"):
+        #Ensures logs directory exists
         self.logs_dir = os.path.join(os.getcwd(), log_dir)
         os.makedirs(self.logs_dir, exist_ok=True)
+        
+        #Timestamped log file(for persistence)
         log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
         self.log_file_path = os.path.join(self.logs_dir, log_file)
 
     def get_logger(self, name=__file__):
         logger_name = os.path.basename(name)
 
+        #Configure logging for console + file (both json)
         file_handler = logging.FileHandler(self.log_file_path)
         file_handler.setLevel(logging.INFO)
         file_handler.setFormatter(logging.Formatter("%(message)s"))
@@ -27,7 +31,8 @@ class CustomLogger:
             format="%(message)s",
             handlers=[console_handler, file_handler]
         )
-
+        
+        #Configure structing for JSON logging
         structlog.configure(
             processors=[
                 structlog.processors.TimeStamper(fmt="iso", utc=True, key="timestamp"),
@@ -40,3 +45,4 @@ class CustomLogger:
         )
 
         return structlog.get_logger(logger_name)
+    
